53ff61b7cab91cfea0a31f029e5431a073b9c40d
qq690195044 <37973621+qq690195044@users.noreply.github.com>
2022-01-30 03:50:44 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -9,7 +9,7 @@ let extraScore = 1;
 let wuDi = true;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 9;
+let firstFruit = 0;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = false;
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = 'https://live.bilibili.com/23369901';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大蝶';
+document.getElementsByTagName("title")[0].innerText = '合成大傻蝶';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
