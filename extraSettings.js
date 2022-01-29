7532e08e5f6c25349c5800fcbaa66281fdc26674
qq690195044 <37973621+qq690195044@users.noreply.github.com>
2022-01-30 03:28:13 +0800

    1

--- extraSettings.js
+++ extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = 'https://live.bilibili.com/23369901';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大蝶蝶';
+document.getElementsByTagName("title")[0].innerText = '合成大蝶';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
